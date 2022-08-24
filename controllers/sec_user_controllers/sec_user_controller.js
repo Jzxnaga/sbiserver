502c49a09e128c7808b302af4c5165ba3fc6e1fd
juan sanjaya <juansanjaya@juans-MacBook-Air.local>
2022-08-24 12:07:29 +0700

    send id on login

--- controllers/sec_user_controllers/sec_user_controller.js
+++ controllers/sec_user_controllers/sec_user_controller.js
@@ -66,7 +66,7 @@ class sec_user_controller{
               // console.log(verify_token)
           Sec_User.update(bodyUpdate,{where:{id:data.dataValues.id}})
           //update token in sec_user as id nanti bisa dihilangkan
-          res.status(200).json({access_token,username,id})
+          res.status(200).json({access_token,username,id:data.dataValues.id})
         }
         else{
           throw ({message: 'INVALID_PASSWORD'})
